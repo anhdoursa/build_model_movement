bf3ab1fc1ba18e4dbd2cc098519a8f5eacaef3a9
anhdoursa <anh.do@ursa.vn>
2023-04-28 10:53:25 +0700

    .

---
+++ precache-manifest.823d6a34f1fee2be14d35cef27c3a60b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "67c0a9d57922b52a586f",
+    "url": "/static/js/main.4eaffdec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3f12a4e9f8674e195715",
+    "url": "/static/js/2.9431556d.chunk.js"
+  },
+  {
+    "revision": "4d7850eb07e5e2fcc11395175fdf3ae0",
+    "url": "/index.html"
+  }
+];
